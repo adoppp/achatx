7d4e89d55ddf6a2b26cfe13e8d22630568cfe864
adoppp <chdn786od@gmail.com>
2026-06-06 13:20:34 +0200

    base url for vite and package.json added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/AChatX/',
     plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
     resolve: {
         alias: {
